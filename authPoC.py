032ba5e0d8a25906175aaed89501a8a973f79b07
lolblat <orr12orr@gmail.com>
2019-05-02 07:04:37 +0300

    backup

---
+++ authPoC.py
@@ -0,0 +1,37 @@
+from idaapi import Form, warning
+import requests
+import jwt
+BASE_URL = "https://our-amazon-web.com/"
+LOGIN_PATH = "api/users/tokens"
+
+class MyForm(Form):
+    def __init__(self):
+        self.invert = False
+        Form.__init__(self, r"""STARTITEM {id:iUserName}
+BUTTON YES* Login
+BUTTON CANCEL Cancel
+Login form
+{FormChangeCb}
+<##Enter your username:{iUserName}>
+<##Enter your password:{iPassword}>
+{iResult}
+""", {
+            'FormChangeCb': Form.FormChangeCb(self.OnFormChange),
+            'iUserName': Form.StringInput(tp=Form.FT_ASCII),
+            'iPassword': Form.StringInput(tp=Form.FT_ASCII),
+            "iResult": Form.StringLabel(tp=Form.FT_ASCII, value="", sz=1024)
+        })
+    def OnFormChange(self, fid):
+        if fid == -2: # click on ok
+            print "Clicked on login"
+            user_token = try_to_log_in(self.GetControlValue(self.iUserName), self.GetControlValue(self.iPassword))
+            if user_token == "-1":
+                warning("Error, unvalid username and password")
+            else:
+                return 1
+        else:
+            return 1
+
+def try_to_log_in(username,password):
+    return requests.post("{0}{1}".format(BASE_URL, LOGIN_PATH), data={"username":username, "password": password}).content
+    
---
+++ communication_manager.py
@@ -0,0 +1,30 @@
+import jwt
+import argparse
+from queue import Queue
+import requests
+from threading import Thread
+
+SECRECT_KEY = "RkK1EM4xHRUOfYBXvrw4"
+EVENT_QUEUE = Queue()
+
+def pulling(user_token):
+	pass
+
+def pass_to_ida():
+
+	pass
+
+def parse_args():
+	parser = argparse.ArgumentParser(description="Puller for the IDA Plugin IReal")
+	parser.add_argument("token", type=str, description="User token")
+	return parser.parse().token
+
+def main(user_token):
+	pulling_thread = Thread(target = pulling, args = (user_token, ))
+	pulling_thread.start()
+	pass_to_ida_thread = Thread(target = pass_to_ida)
+	pass_to_ida_thread.start()
+	
+if __name__ == "__main__":
+	user_token = parse_args()
+	main(user_token)
